8ee452cc940dd38338628c779fd1d422cdba631d
Eugene <levgenii.rachkov@gettyimages.com>
2020-12-19 10:20:25 +0200

    fixed formula

--- lesson1/hw2.py
+++ lesson1/hw2.py
@@ -1,8 +1,8 @@
 def calc_quadratic_equations(a,b,c):  
     D = b**2 - 4*a*c
     if (D>0):
-        x1 = (-b + D**0.5) / 2*a
-        x2 = (-b - D**0.5) / 2*a
+        x1 = (-b + D**0.5) / (2*a)
+        x2 = (-b - D**0.5) / (2*a)
         return x1, x2
     elif  (D==0):
         x1 = -b / (2 * a)
